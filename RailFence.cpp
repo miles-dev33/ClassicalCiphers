f580fccb82d9169e5e347f0c42984bd9868950e8
milesjmccloskey <milesjmccloskey@gmail.com>
2018-06-04 00:27:16 -0700

    ClassicalCiphers

---
+++ RailFence.cpp
@@ -0,0 +1,122 @@
+#include "RailFence.h"
+
+
+/**
+ * Sets the key to use
+ * @param key - the key to use
+ * @return - True if the key is valid and False otherwise
+ */
+bool RailFence::setKey(const string& key)
+{
+	RailFence railfence;
+  int Ckey = stoi(key);
+  if (!(isdigit[Ckey]))
+    return false;
+	else{
+    railfence.key = key;
+    return true;
+  }
+}
+
+/**
+ * Encrypts a plaintext string
+ * @param plaintext - the plaintext string
+ * @return - the encrypted ciphertext string
+ */
+string RailFence::encrypt(const string& plaintext)
+{
+	string result;
+
+	RailFence railfence;
+	int key = railfence.key;
+	if ((plaintext.length()%key) > 0) {
+		int row = key;
+		int col = (plaintext.length()/key)+ 1;
+		char rail[row][col] = {*};
+	}
+	else{
+		int row = key;
+		int col = (plaintext.length()/key);
+		char rail[row][col] = {*};
+	}
+
+
+	int k = 0;
+	do {
+
+		for (int i = 0; i < col; i++) {
+			for (int j = 0; j < key; j++) {
+				rail[j][i] = plaintext[k];
+				k++;
+			}
+		}
+	} while(k < plaintext.length());
+
+	int g = 0;
+	do {
+		for (int i = 0; i < key; i++) {
+			for (int j = 0; j < col; i++) {
+				if (rail[i][j] == "*") {
+					g++
+				}
+				else {
+					result[g] = rail[i][j];
+					g++;
+				}
+			}
+		}
+	} while(g < plaintext.length());
+
+return result;
+}
+
+/**
+ * Decrypts a string of ciphertext
+ * @param cipherText - the ciphertext
+ * @return - the plaintext
+ */
+string RailFence::decrypt(const string& cipherText)
+{
+	string result;
+
+	RailFence railfence;
+	int key = railfence.key;
+	if ((cipherText.length()%key) > 0) {
+		int row = key;
+		int col = (cipherText.length()/key)+ 1;
+		char rail[row][col] = {*};
+	}
+	else{
+		int row = key;
+		int col = (cipherText.length()/key);
+		char rail[row][col] = {*};
+	}
+
+	int k = 0;
+	do {
+		for (int i = 0; i < key; i++) {
+			for (int j = 0; j < col; j++) {
+				rail[i][j] = plaintext[k];
+				k++;
+			}
+		}
+	} while(k < plaintext.length());
+
+	int g = 0;
+	do {
+		for (int i = 0; i < col; i++) {
+			for (int j = 0; j < key; i++) {
+				if (rail[j][i] == "*") {
+					g++;
+				}
+				else {
+					result[g] = rail[j][i];
+					g++;
+				}
+			}
+		}
+	} while(g < cipherText.length());
+
+return result;
+
+}
---
+++ cipher.cpp
@@ -0,0 +1,50 @@
+#include <string>
+#include "CipherInterface.h"
+#include "Playfair.h"
+#include "Caesar.h"
+#include "Railfence.h"
+#include "RowTransposition.h"
+#include "Vigenre.h"
+#include <fstream>
+
+
+#ifndef __HP_aCC
+using namespace std;
+#endif
+
+int main(int argc, char** argv)
+{
+		if(argc != 5)
+        {
+        //    cout <<"There aren't enough arguments in the command\n";
+            return 1;
+        }
+    string cipherName = argv[0];
+    string key = argv[1];
+    string encOrDec = argv[2];
+    string inputFile = argv[3];
+    string outputFile = argv[4];
+
+    CipherInterface* cipher = NULL;
+
+
+    if(cipherName == "PLF")
+    {
+        /* Create an instance of the Playfair cipher */
+        cipher = new Playfair();
+        /* Error checks */
+        if(!cipher)
+        {
+            fprintf(stderr, "ERROR [%s %s %d]: could not allocate memory\n",
+                    __FILE__, __FUNCTION__, __LINE__);
+            exit(-1);
+        }
+        /* Set the encryption key */
+        cipher->setKey(key);
+        /* Perform encryption */
+        string cipherText = cipher->encrypt("hello world");
+        /* Perform decryption */
+        cipher->decrypt(cipherText);
+    }
+	return 0;
+}
